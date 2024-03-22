4c60fc4532e4335702c43338bef388dd88a3806e
Rohit-del-source <143836052+Rohit-del-source@users.noreply.github.com>
2024-03-22 14:23:20 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,6 +11,16 @@ const userDetails = JSON.parse(
 app.use(express.json());
 
 //Write DELETE endpoint for deleting the details of user
+app.delete("/api/v1/details/:id", (req,res)=>{
+   const id = req.params.id;
+   const user = userDetails.find((el)=> el.id === parseInt(id));
+   if(!user){
+     return res.status(404).send({ "status": "failed", "message": "User not found!" });
+   }
+   const restUser = userDetails.filter((el)=> el.id !==  parseInt(id));
+    fs.writeFileSync(`${__dirname}/data/userDetails.json`,JSON.stringify(restUser));
+    res.status(200).send({"status": "success","message": "User details deleted successfully", "data": {"details": user}})
+})
 
 // PATCH endpoint for editing user details
 app.patch("/api/v1/details/:id", (req, res) => {
